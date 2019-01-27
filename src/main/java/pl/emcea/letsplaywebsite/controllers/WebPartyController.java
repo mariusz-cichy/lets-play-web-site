3a4e38e229847bda9a77d11caaa7a9275523e188
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2019-01-27 14:08:42 +0100

    Pierwszy commit. Nie jest to pusty projekt. Zawiera już kilka podstawowych funkcjonalności po refaktoringu.

---
+++ src/main/java/pl/emcea/letsplaywebsite/controllers/WebPartyController.java
@@ -0,0 +1,46 @@
+package pl.emcea.letsplaywebsite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.emcea.letsplaywebsite.models.Greeting;
+
+@Controller
+public class WebPartyController {
+
+    @RequestMapping({"", "/"})
+    public String homePage() {
+        return "homePage";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        return "aboutPage";
+    }
+
+    @GetMapping("/basket")
+    public String basketPage(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "basketPage";
+    }
+
+    @PostMapping("/basket")
+    public String basketSubmitPage(@ModelAttribute Greeting greeting) {
+        return "basketPage";
+    }
+
+
+    @RequestMapping("/login")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @RequestMapping("/register")
+    public String registerPage() {
+        return "registerPage";
+    }
+
+}
