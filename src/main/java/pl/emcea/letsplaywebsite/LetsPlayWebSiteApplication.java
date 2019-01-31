cbcbb9a0314ada336a8fc5884d952eb8efc6d3ee
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2019-01-31 10:28:21 +0100

    Dodałem pozostałe elementy dotyczące Security.

--- src/main/java/pl/emcea/letsplaywebsite/LetsPlayWebSiteApplication.java
+++ src/main/java/pl/emcea/letsplaywebsite/LetsPlayWebSiteApplication.java
@@ -2,9 +2,16 @@ package pl.emcea.letsplaywebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LetsPlayWebSiteApplication {
+public class LetsPlayWebSiteApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LetsPlayWebSiteApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LetsPlayWebSiteApplication.class, args);
