235c8c6e34227c71ad1cb8717f6f03c3efe9e852
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2019-02-03 23:29:13 +0100

    Tutaj są problemy z Security. Trzzeba to przejrzeć o poprawić.

--- src/main/java/pl/emcea/letsplaywebsite/controllers/WebPartyController.java
+++ src/main/java/pl/emcea/letsplaywebsite/controllers/WebPartyController.java
@@ -8,39 +8,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.emcea.letsplaywebsite.models.Greeting;
 
-@Controller
-public class WebPartyController {
-
-    @RequestMapping({"", "/"})
-    public String homePage() {
-        return "homePage";
-    }
-
-    @RequestMapping("/about")
-    public String aboutPage() {
-        return "aboutPage";
-    }
-
-    @GetMapping("/basket")
-    public String basketPage(Model model) {
-        model.addAttribute("greeting", new Greeting());
-        return "basketPage";
-    }
-
-    @PostMapping("/basket")
-    public String basketSubmitPage(@ModelAttribute Greeting greeting) {
-        return "basketPage";
-    }
-
-
-//    @RequestMapping("/login")
-//    public String loginPage() {
-//        return "loginPage";
+//@Controller
+//public class WebPartyController {
+//
+//    @RequestMapping({"", "/"})
+//    public String homePage() {
+//        return "homePage";
 //    }
 //
-//    @RequestMapping("/register")
-//    public String registerPage() {
-//        return "registerPage";
+//    @RequestMapping("/about")
+//    public String aboutPage() {
+//        return "aboutPage";
 //    }
-
-}
+//
+//    @GetMapping("/basket")
+//    public String basketPage(Model model) {
+//        model.addAttribute("greeting", new Greeting());
+//        return "basketPage";
+//    }
+//    @PostMapping("/basket")
+//    public String basketSubmitPage(@ModelAttribute Greeting greeting) {
+//        return "basketPage";
+//    }
+//}
