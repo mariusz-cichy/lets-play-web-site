510f80d1bdc876331bff76c0fd109f855d51488d
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2019-03-05 18:56:44 +0100

    + dodałem model: Item + dodałem dane testowe (EN) dla Items w data.sql

---
+++ src/main/java/pl/emcea/letsplaywebsite/models/Item.java
@@ -0,0 +1,102 @@
+package pl.emcea.letsplaywebsite.models;
+
+import javax.persistence.*;
+
+@Entity(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String celebrations;
+    private String name;
+    private String description;
+    @Lob
+    private String details;
+    private Double buy;
+    private Double hire;
+    private Integer stock_buy;
+    private Integer stock_hire;
+
+    public Item(String celebrations, String name, String description, String details, Double buy, Double hire, Integer stock_buy, Integer stock_hire) {
+        this.celebrations = celebrations;
+        this.name = name;
+        this.description = description;
+        this.details = details;
+        this.buy = buy;
+        this.hire = hire;
+        this.stock_buy = stock_buy;
+        this.stock_hire = stock_hire;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCelebrations() {
+        return celebrations;
+    }
+
+    public void setCelebrations(String celebrations) {
+        this.celebrations = celebrations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Double getBuy() {
+        return buy;
+    }
+
+    public void setBuy(Double buy) {
+        this.buy = buy;
+    }
+
+    public Double getHire() {
+        return hire;
+    }
+
+    public void setHire(Double hire) {
+        this.hire = hire;
+    }
+
+    public Integer getStock_buy() {
+        return stock_buy;
+    }
+
+    public void setStock_buy(Integer stock_buy) {
+        this.stock_buy = stock_buy;
+    }
+
+    public Integer getStock_hire() {
+        return stock_hire;
+    }
+
+    public void setStock_hire(Integer stock_hire) {
+        this.stock_hire = stock_hire;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -103,3 +103,84 @@ INSERT INTO pool_answers(pool_id, answer, votes)
 VALUES (7, "Słaba", 1);
 INSERT INTO pool_answers(pool_id, answer, votes)
 VALUES (7, "Lepiej nie mówić", 0);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (1, "Anniversary", "Movado Watch", "Movado ladies watch with pink dial",
+"Made of solid, two-tone stainless steel, this polished women’s watch with smoothly sculpted lines features removable links at the back for an adjustable fit. Available with sparkling diamonds and a pink dial.",
+200.00, NULL, 5, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (2, "Anniversary", "Diamond Ring", "Our 18k Gold Diamond Ring is unique and gorgeous",
+"Our 18k gold diamond ring is unique and gorgeous. The sevenstone diamond wedding and anniversary bands are the purest symbol of true love representing the journey you travelled since the moment you met, the experience you are living now, and the unimagined excitement that your future holds.",
+700.00, NULL, 5, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (3, "Anniversary", "Titan Watch", "A beautiful watch from Titan",
+"Quartz movement, dress watch, polished gold-tone stainless steel bezel and crown, stainless steel case and case back, mineral crystal, 30 metres/100 feet water resistant.",
+200.00, NULL, 5, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (4, "Anniversary", "Ring for her and Ring for him", "A perfect Anniversary Gift",
+"A gorgeous 18k gold diamond ring with seven stone diamonds for her and a simple 18k gold ring for him. This is a perfect gift for an anniversary.",
+1000.00, NULL, 5, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (5, "Birthday", "iPhone 4", "Apple iPhone 4 Black 32GB Mobile Phone - NEW - Unlocked",
+"iPhone 4 features a 5 megapixel autofocus camera with a 5x digital zoom, a backside illuminated sensor and built-in LED flash that allows you to take amazing pictures even in low light and dark environments. iPhone 4 lets you record and edit incredible HD video and the popular tap to focus feature now works while recording video. You can use the iPhone 4‘s LED flash for both still photography and video recording. The iMovie® app for iPhone lets you combine movie clips, add dynamic transitions and themes and include photos and music, and users can buy it through the App Store right on their phone. The iPhone 4 includes Multitasking, Folders, enhanced Mail, deeper Enterprise support and Apple’s iAd mobile advertising platform. With Multitasking, users can now instantly switch between any of their apps while preserving battery life. With Folders, users can easily organize their apps into collections by simply dragging one app on top of another.",
+859.00, NULL, 5, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (6, "Birthday", "Birthday Cake", "Wiggles Birthday cake to make your kid feel special",
+"For that special occasion you only want the best. This cake is for your 'Little Wiggle' who would love the mouthwatering white chocolate cake with the lovely topping featuring the Wiggles in their big red car. Specially crafted to the highest quality at a reasonable price.",
+200.00, NULL, 0, NULL);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (7, "Birthday", "Flower Bouquet & Vase", "Surprise your loved ones with flowers",
+"Basket of mixed flowers to make the birthday girl feel really special. Surprise your loved one with the gift that she would love.",
+50.00, 10.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (8, "Christmas", "Christmas Tree", "Delux quality Christmas Tree for home or office",
+"Deluxe green Majestic Pine Traditional Christmas Tree with metal hinged branches. Green Metal Stand. Bulky item. Size: 3.00 metres high.",
+300.00, 20.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (9, "Christmas", "Christmas Trees", "Delux quality Christmas Trees for home or office",
+"Deluxe green Majestic Pine Traditional Christmas Trees with metal hinged branches. Green Metal Stand. Bulky item. Size: 2.1 meters high",
+500.00, 50.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (10, "Christmas", "Christmas Decoration", "Silver Jewel Ball Decoration for your Christmas tree",
+"Silver acrylic ball with diamontes and silver glitter hand painted decoration. Size: 80mm diameter",
+10.00, 2.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (11, "Christmas", "Christmas Decoration", "Silver & Blue Ball Decoration for your Christmas tree",
+"Silver & Blue acrylic ball with diamontes and colored glitter hand painted decoration. Size: 80mm diameter",
+10.00, 2.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (12, "Corporate", "Compendium", "Diary/Organize r/Compendium for office use",
+"Smooth leather-looking exterior with loop closure. Available in black & comes complete with mouse pad, note & card pockets, pen holder & lined notepad.",
+20.00, 2.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (13, "Corporate", "Mouse Pad", "Basic Mouse Pad",
+"Thin, hard surface offers improved tracking and responsiveness. Non-skid rubber base protects desktops from scuffs and scratches. Durable polyester construction. Size: 239 x 190 mm",
+7.00, 2.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (14, "Corporate", "Mug", "Classic Coffee Mug",
+"Perfect for home or office, the coffee mug is microwave and dishwasher safe.",
+7.00, 1.00, 5, 3);
+
+INSERT INTO items (id, celebrations, name, description, details, buy, hire, stock_buy, stock_hire)
+VALUES (15, "Corporate", "Pen", "Classic Pen",
+"Quality pens at an affordable price. Nib Size 1.0mm. Pack of 50",
+25.00, NULL, 5, NULL);
+
+
+
+
+
+
